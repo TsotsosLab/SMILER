6f491c98dea5af96713c387cbe87b7e0e4184e00
tkunic <tkunic@rocketmail.com>
2019-03-12 17:06:14 -0400

    Clean up MLNet code and fix 'center_prior: none' condition.

--- models/docker/MLNet/model/run_model.py
+++ models/docker/MLNet/model/run_model.py
@@ -4,12 +4,12 @@
 from __future__ import division
 
 import os
-from os import listdir
-from os.path import isfile, join
+import json
 os.environ['KERAS_BACKEND'] = 'theano'
 
 from keras.optimizers import SGD
 from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
 import cv2, sys
 import numpy as np
 from config import *
@@ -18,78 +18,37 @@ from model import ml_net_model, loss
 
 from smiler_tools.runner import run_model
 
-
-def generator(b_s, phase_gen='train'):
-    if phase_gen == 'train':
-        images = [
-            join(imgs_train_path, f) for f in listdir(imgs_train_path)
-            if isfile(join(imgs_train_path, f))
-        ]
-        maps = [
-            join(maps_train_path, f) for f in listdir(maps_train_path)
-            if isfile(join(maps_train_path, f))
-        ]
-    elif phase_gen == 'val':
-        images = [
-            join(imgs_val_path, f) for f in listdir(imgs_val_path)
-            if isfile(join(imgs_val_path, f))
-        ]
-        maps = [
-            join(maps_val_path, f) for f in listdir(maps_val_path)
-            if isfile(join(maps_val_path, f))
-        ]
-    else:
-        raise NotImplementedError
-
-    images.sort()
-    maps.sort()
-
-    counter = 0
-    while True:
-        yield preprocess_images(
-            images[counter:counter + b_s], shape_r, shape_c), preprocess_maps(
-                maps[counter:counter + b_s], shape_r_gt, shape_c_gt)
-        counter = (counter + b_s) % len(images)
-
-
 if __name__ == '__main__':
-    phase = 'test'
+    options = json.loads(os.environ['SMILER_PARAMETER_MAP'])
+    use_default_center_bias = options.get('center_prior',
+                                          'default') == 'default'
 
     model = ml_net_model(
         img_cols=shape_c, img_rows=shape_r, downsampling_factor_product=10)
     sgd = SGD(lr=1e-3, decay=0.0005, momentum=0.9, nesterov=True)
+
     print("Compiling ML-Net Model...")
     model.compile(sgd, loss)
     print("Done")
 
-    if phase == 'train':
-        print("Training ML-Net")
-        model.fit_generator(
-            generator(b_s=b_s),
-            nb_imgs_train,
-            nb_epoch=nb_epoch,
-            validation_data=generator(b_s=b_s, phase_gen='val'),
-            nb_val_samples=nb_imgs_val,
-            callbacks=[
-                EarlyStopping(patience=5),
-                ModelCheckpoint(
-                    'weights.mlnet.{epoch:02d}-{val_loss:.4f}.pkl',
-                    save_best_only=True)
-            ])
+    model.load_weights('mlnet_salicon_weights.pkl')
 
-    elif phase == "test":
-        model.load_weights('mlnet_salicon_weights.pkl')
-
-        def compute_saliency(img_path):
+    def compute_saliency(image_path):
+        if use_default_center_bias:
             pred = model.predict_on_batch(
-                preprocess_images([img_path], shape_r, shape_c))
+                preprocess_images([image_path], shape_r, shape_c))
             sm = pred[0][0]
+        else:
+            get_unbiased_output = K.function(
+                [model.layers[0].input,
+                 K.learning_phase()], [model.layers[21].output])
+            pred = get_unbiased_output(
+                [preprocess_images([image_path], shape_r, shape_c), 0])
+            sm = pred[0][0][0]
 
-            original_image = cv2.imread(img_path, 0)
-            sm = postprocess_predictions(sm, original_image.shape[0],
-                                         original_image.shape[1])
-            return sm
+        original_image = cv2.imread(image_path, 0)
+        sm = postprocess_predictions(sm, original_image.shape[0],
+                                     original_image.shape[1])
+        return sm
 
-        run_model(compute_saliency)
-    else:
-        raise NotImplementedError
+    run_model(compute_saliency)
